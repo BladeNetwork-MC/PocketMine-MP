f03785fc029d0c5fede4e11090541cbffebada8f
Revand Mika <82688238+RevandDev@users.noreply.github.com>
2024-07-15 22:00:02 +0800

    Update WindExplosionParticle.php

--- src/world/particle/WindExplosionParticle.php
+++ src/world/particle/WindExplosionParticle.php
@@ -27,9 +27,9 @@ use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\protocol\LevelEventPacket;
 use pocketmine\network\mcpe\protocol\types\ParticleIds;
 
-class WindExplosionParticle implements Particle{
+class WindExplosionParticle extends ProtocolParticle{
 
 	public function encode(Vector3 $pos) : array{
-		return [LevelEventPacket::standardParticle(ParticleIds::WIND_EXPLOSION, 0, $pos)];
+		return [LevelEventPacket::standardParticle(ParticleIds::WIND_EXPLOSION, 0, $this->protocolId, $pos)];
 	}
 }
